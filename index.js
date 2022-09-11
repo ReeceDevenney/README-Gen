d92eca56aae15da920b27383a4e15c03e95ec6db
Reece Devenney <reecedevenney@gmail.com>
2022-09-11 18:58:21 -0400

    added question prompts

--- index.js
+++ index.js
@@ -1,7 +1,52 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer')
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptinfo = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'username',
+            message: 'What is your Github username?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'what is the description of your project?',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What are the installation instructions of your project?',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What is the usage of your project?',
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'What are teh contribution guidelines of your project?',
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'What are the test instructions of your project?',
+        }
+    ])
+}
+
+promptinfo()
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
